9b333fc74ee8c83a3a0e42066fe71e705eee51fd
whiteHeart313 <ammarhamed64@gmail.com>
2025-07-17 21:44:28 +0300

    feat: initialize React application with routing and TailwindCSS styling

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import BookService from './pages/BookService';
+import MyBookings from './pages/MyBookings';
+import Contact from './pages/Contact';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/book" element={<BookService />} />
+          <Route path="/bookings" element={<MyBookings />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
